55d081d887fc492bb29183777bcd1ce4eca334f6
Erik Nylund <erik.nylund@campusnykoping.se>
2024-10-28 12:03:52 +0100

    Metoder som genererar registreringsnummer

---
+++ TheGears.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeluxeParking
+{
+    internal class TheGears
+    {
+        public string VehicleRegistration()
+        {
+            string vehicleReg = LetterGenerator();
+            vehicleReg = vehicleReg + NumberGenerator();
+            return vehicleReg;
+        }
+
+        public string LetterGenerator()
+        {
+            string regLetters = "";
+            
+            for (int i = 0; i < 3; i++)
+            {
+                int letterIndex = Random.Shared.Next(0, 26);
+                char letter = (char)('A' + letterIndex);
+                regLetters = regLetters + letter;
+            }         
+            return regLetters;
+        }
+
+        public string NumberGenerator()
+        {
+            string regNumber = "";
+            
+            for (int i = 0; i < 3; i++) 
+            {
+                int number = Random.Shared.Next(0, 10);
+                regNumber = regNumber + number;
+            }
+            return regNumber;
+        }
+
+    }
+}
