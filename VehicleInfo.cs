8fc7b669459744c9805e90ab7d48cf60d5e115b2
Erik Nylund <erik.nylund@campusnykoping.se>
2024-10-28 11:23:20 +0100

    Skapa klass med info för fordon

---
+++ VehicleInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeluxeParking
+{
+    internal class VehicleInfo
+    {
+        public string[] colors = {
+            "Red", "Blue", "Green", "Yellow", "Black",
+            "White", "Gray", "Orange", "Purple",
+            "Pink", "Brown", "Turquoise", "Magenta",
+            "Beige", "Gold", "Silver", "Navy Blue"
+        };
+
+
+        public string[] motorcycleBrands = {
+            "Harley-Davidson", "Yamaha", "Honda", "Kawasaki",
+            "Ducati", "BMW", "Suzuki", "Triumph",
+            "KTM", "Aprilia", "Indian", "MV Agusta",
+            "Royal Enfield", "Moto Guzzi", "Husqvarna", "Benelli"
+        };
+    }
+}
